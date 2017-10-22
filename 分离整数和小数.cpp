08a273ed82da330519d7414e38e458c73ea2610d
fancyGenTatsu <32222628+fancyGenTatsu@users.noreply.github.com>
2017-10-22 16:57:11 +0800

    Add files via upload

---
+++ 分离整数和小数.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+ {
+ 	float x;
+ 	printf("«Î ‰»Î±‰¡øx£∫\n");
+ 	scanf("%f",&x);
+ 	int a;
+ 	double b;
+ 	a=x;
+	b=x-a; 
+	 printf("%d\t%lf\t",a,b);
+ 	return 0;
+}
+ 
