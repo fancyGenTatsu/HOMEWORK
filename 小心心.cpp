3acbdcdd3dd42d92f9b7cdcccb854c3902aad3db
fancyGenTatsu <32222628+fancyGenTatsu@users.noreply.github.com>
2017-11-30 00:12:11 +0800

    Add files via upload

---
+++ 小心心.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+	int m,n,i,j,k;
+	scanf("%d %d",&m,&n);
+	if(m>=1&&m<=5&&n>=1&&n<=5)
+	{
+		for(j=0;j<m;j++)
+		
+		{
+		for(i=0;i<n;i++)
+		{
+		        printf("--**-**--");
+		        
+		}printf("\n");
+		for(i=0;i<n;i++)
+		{
+		    	printf("-*--*--*-");
+		}
+	printf("\n");	
+	for(i=0;i<n;i++)
+		{
+		    	printf("-*-----*-");
+		}
+	printf("\n");
+	
+	for(i=0;i<n;i++)
+		{
+		    	printf("--*---*--");
+		}
+	printf("\n");
+	for(i=0;i<n;i++)
+		{
+		    	printf("---*-*---");
+		}
+	printf("\n");
+	for(i=0;i<n;i++)
+		{
+		    	printf("----*----");
+		}
+	printf("\n");
+	}
+	}
+	else
+	printf("Input Error");
+return 0;	
+}
