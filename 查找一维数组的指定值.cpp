07ba65ac26739974fc26039ae916fe8b8372254c
fancyGenTatsu <32222628+fancyGenTatsu@users.noreply.github.com>
2017-11-30 00:20:22 +0800

    Add files via upload

---
+++ 查找一维数组的指定值.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,m;
+	scanf("%d",&n);
+	int a[5];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&m);
+	for(j=0;j<n;j++)
+	{
+		if(a[j]==m)
+		printf("%d\t",j);
+		else;
+	}
+return 0;
+}
